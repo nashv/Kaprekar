a236c95838b3aabc81869e5f4b703024e70b3276
nashv <nash@vartak.org>
2024-01-18 00:54:46 +0100

    Added step counting

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 start = 1
 end = 10000
 
+
 def differenceofsorted(x):
+    global steps
     a = int("".join(sorted(str(x))).strip())
     d = int("".join(sorted(str(x), reverse=True)).strip())
     q = d - a
     print(x, " : ", d, " - ", a, "= ", q)
-    if (q == 6174):  # 4-digit Kaprekar constant
-        print("Goal Reached for: ", x)
-    elif (q == 495):  # 3-digit Kaprekar constant
-        print("Goal Reached for: ", x)
-    elif (q == 0):
-        print("Zero difference encountered for :", x)
+    if q == 6174:  # 4-digit Kaprekar constant
+        print("Goal Reached for: ", x, "in ", steps, " steps")
+    elif q == 495:  # 3-digit Kaprekar constant
+        print("Goal Reached for: ", x, "in ", steps, " steps")
+    elif q == 0:
+        print("Zero difference encountered for :", x, "in ", steps, " steps")
     else:
-        x = differenceofsorted(q)
+        differenceofsorted(q)
+
+        steps += 1
+    return ()
 
 
 # Loop through all 4-digit numbers
 for n in range(start, end):
+    steps = 1
     differenceofsorted(n)
