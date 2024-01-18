f3ccb5251891b576fa748dbd02420dfc85089468
nashv <nash@vartak.org>
2024-01-18 01:12:27 +0100

    Added step counting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 start = 1
 end = 10000
 
+stepdict = []
+
 
 def differenceofsorted(x):
     global steps
@@ -15,13 +17,13 @@ def differenceofsorted(x):
     elif q == 0:
         print("Zero difference encountered for :", x, "in ", steps, " steps")
     else:
+        steps += 1  # increment step counter
         differenceofsorted(q)
-
-        steps += 1
-    return ()
+    return steps
 
 
 # Loop through all 4-digit numbers
 for n in range(start, end):
     steps = 1
     differenceofsorted(n)
+    stepdict.append(steps)
